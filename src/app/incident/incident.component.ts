b4d311e9dec01ec275212fd38846ecd7813c483f
Chris Faria <chrisfaria@gmail.com>
2019-08-12 16:30:36 -0400

    Updated the top menu

--- src/app/incident/incident.component.ts
+++ src/app/incident/incident.component.ts
@@ -22,6 +22,8 @@ export class IncidentComponent implements OnInit {
 
   today = new Date();
   testnote = true;
+  valueLabel1 = '';
+  valueLabel2 = '';
 
   names: Name[] = [
     {value: '001-Manpreet', viewValue: 'Manpreet'},
@@ -33,16 +35,16 @@ export class IncidentComponent implements OnInit {
 
   types: IncidentType[] = [
     {value: '001-LateArrival', viewValue: 'Late arrival'},
-    {value: '002-Leave early', viewValue: 'Leave early'},
+    {value: '002-LeaveEarly', viewValue: 'Leave early'},
     {value: '003-Vacation', viewValue: 'Vacation'},
-    {value: '004-Sick day', viewValue: 'Sick day'},
-    {value: '005-Altered schedule', viewValue: 'Altered schedule'},
-    {value: '006-Long lunch', viewValue: 'Long lunch'},
-    {value: '007-Work from home', viewValue: 'Work from home'},
-    {value: '008-Lieu day', viewValue: 'Lieu day'},
+    {value: '004-SickDay', viewValue: 'Sick day'},
+    {value: '005-AlteredSchedule', viewValue: 'Altered schedule'},
+    {value: '006-LongLunch', viewValue: 'Long lunch'},
+    {value: '007-WorkFromHome', viewValue: 'Work from home'},
+    {value: '008-LieuDay', viewValue: 'Lieu day'},
     {value: '009-Overtime', viewValue: 'Overtime'},
-    {value: '011-Personal day', viewValue: 'Personal day'},
-    {value: '010-Leave of absence', viewValue: 'Leave of absence'},
+    {value: '011-PersonalDay', viewValue: 'Personal day'},
+    {value: '010-LeaveOfAbsence', viewValue: 'Leave of absence'},
     {value: '012-Note', viewValue: 'Note'},
     {value: '013-Event', viewValue: 'Event'}
   ];
@@ -57,9 +59,6 @@ export class IncidentComponent implements OnInit {
     days: [''],
     //date: [this.today, Validators.pattern('^(([1-9])|((1)[0-2]))(\/)([1-9]|[1-2][0-9]|(3)[0-1])(\/)\d{4}$')],
     //date: [this.today, Validators.pattern('^\d{4}(-)(((0)[0-9])|((1)[0-2]))(-)([0-2][0-9]|(3)[0-1]).*$')],
-    details: this.fb.array([
-      this.fb.control('')
-    ]),
     note: ['']
   });
 
@@ -89,10 +88,49 @@ export class IncidentComponent implements OnInit {
     this.details.push(this.fb.control(''));
   }
 
-  checkValue()
+  isTimeType()
   {
-    if (this.incidentForm.value.type == '003-Vacation')
+    if (this.incidentForm.value.type == '001-LateArrival' || 
+        this.incidentForm.value.type == '002-LeaveEarly')
     {
+      this.valueLabel1 = 'Time'
+      return true;
+    }
+    return false;
+  }
+
+  isStartEndTimeType()
+  {
+    if (this.incidentForm.value.type == '005-AlteredSchedule')
+    {
+      this.valueLabel1 = 'Start Time'
+      this.valueLabel1 = 'End Time'
+      return true;
+    }
+    return false;
+  }
+
+  isHoursType()
+  {
+    if (this.incidentForm.value.type == '006-LongLunch')
+    {
+      this.valueLabel1 = 'Hours'
+      return true;
+    }
+    return false;
+  }
+
+  isDaysType()
+  {
+    if (this.incidentForm.value.type == '003-Vacation' ||
+        this.incidentForm.value.type == '004-SickDay' ||
+        this.incidentForm.value.type == '007-WorkFromHome' ||
+        this.incidentForm.value.type == '008-LieuDay' ||
+        this.incidentForm.value.type == '009-Overtime' ||
+        this.incidentForm.value.type == '011-PersonalDay' ||
+        this.incidentForm.value.type == '010-LeaveOfAbsence')
+    {
+      this.valueLabel1 = 'Days'
       return true;
     }
     return false;
